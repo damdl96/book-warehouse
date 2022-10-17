7374e3db39e414442d689b0af55d8ffd94613c0c
Diego Moreno <diegodm96@hotmail.com>
2022-10-17 16:21:57 -0400

    remove unnecessary information on class comment

--- db/migrate/20221017172502_create_stores.rb
+++ db/migrate/20221017172502_create_stores.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Migration to create stores table on DB
+# Migration to create stores table
 class CreateStores < ActiveRecord::Migration[7.0]
   def change
     create_table :stores do |t|
