e80da6b41e217cbf31665ab28f588d3794ae7c5e
Diego Moreno <diegodm96@hotmail.com>
2022-10-17 15:29:38 -0400

    Add missing constraint in books migration

--- db/migrate/20221017145716_create_books.rb
+++ db/migrate/20221017145716_create_books.rb
@@ -4,9 +4,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
-      t.string :genre
+      t.string :title, nil: false
+      t.string :author, nil: false
+      t.string :genre, nil: false
 
       t.timestamps
     end
