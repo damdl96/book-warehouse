3797dbc956d75d34d360eeedcf3f1409caea5757
Diego Moreno <diegodm96@hotmail.com>
2022-10-19 10:38:14 -0400

    Configurations to support admin and API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  include DeviseTokenAuth::Concerns::SetUserByToken
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
-  end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
 end
--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-class InventoriesController < ApplicationController
+class InventoriesController < ApiController
 end
