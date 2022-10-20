a888da6b41061a32c6a8604a85c556edef576c73
Diego Moreno <diegodm96@hotmail.com>
2022-10-20 17:29:09 -0400

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :inventories
-  resources :books
+  resources :inventories, only: [:index, :create, :update]
+  resources :books, only: [:create, :destroy]
 
   root 'admin/dashboard#index'
 end
