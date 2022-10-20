578df033d006a553017c457e1e7dd8690dd074d9
Diego Moreno <diegodm96@hotmail.com>
2022-10-20 16:34:33 -0400

    Remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :inventories
-  resources :stores
   resources :books
 
   root 'admin/dashboard#index'
